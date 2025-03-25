21417b74121d23739908ebee936beea01c63fc53
Jiya Gupta <coderaholic21@gmail.com>
2025-03-25 13:17:55 +0530

    fix:route to signin page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useSession, signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
@@ -11,12 +11,15 @@ export default function Component() {
     if (session) {
       router.push("/dashboard");
     }
+    else{
+      router.push("/signin");
+    }
   }, [session]);
 
   return (
     <div className="bg-[#ECE6F0] h-screen items-center justify-center flex flex-col">
       <p className="text-black">Not signed in</p>
-      <button className="bg-purple-700 rounded-md text-white" onClick={() => signIn()}>Sign in</button>
+      <button className="bg-purple-700 rounded-md p-1 px-2 text-white" onClick={() => signIn()}>Sign in</button>
     </div>
   );
 }
